3ae127fe02771b2c451ed5d4e8847ce533a1fede
Alberto Benatti <albi.benni8@gmail.com>
2023-06-21 21:33:07 +0200

    rest from app = FastAPI

--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
+from enum import Enum
 from fastapi import FastAPI
-from .routers import method_testing
+from pydantic import BaseModel
 
 app = FastAPI()
 
-app.include_router(method_testing.router)
+
+class Category(Enum):
+    TOOLS = 'tools'
+    CONSUMABLES = 'consumables'
 
 
-if __name__ == "__main__":
-    import uvicorn
+class Item(BaseModel):
+    name: str
+    price: float
+    count: int
+    id: int
+    category: Category
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+
+items = {
+    0: Item(name="Hammer", price=9.99, count=20, id=0, category=Category.TOOLS),
+    1: Item(name="Pliers", price=5.99, count=20, id=1, category=Category.TOOLS),
+    2: Item(name="Nails", price=1.99, count=100, id=2, category=Category.CONSUMABLES),
+}
+
+
+# FastAPI handles JSON serialization and deserialization for us.
+# We can simply use built-in python and Pydantic types, in this case dict[int, Item].
+@app.get("/")
+def index() -> dict[str, dict[int, Item]]:
+    return {"items": items}
